c656dc49393b146f347f349858516389ef159434
Jonathan Chu <jonathan.chu@me.com>
2014-09-15 16:14:11 -0400

    Iterate through a file and remove quotes, write to a new file.

--- remove_quotes/cli.py
+++ remove_quotes/cli.py
@@ -1,9 +1,22 @@
+import os
 import click
 
 
 @click.command()
-@click.option('--filename', '-f', is_flag=True, help='Filename to process.')
+@click.option('--filename', '-f', help='Filename to process.')
 @click.argument('filename', required=True)
-def main(name, filename):
+def main(filename):
     """Removes all quotes in a file."""
-    pass
+    read_filename = filename
+    orig_filename, ext = os.path.splitext(read_filename)
+    write_filename = orig_filename + '_new{}'.format(ext)
+
+    write_buffer = open(write_filename, 'wb')
+
+    with open(read_filename, 'r') as f:
+        for line in f:
+            line = line.replace('"', '').replace("'", "")
+            write_buffer.write(bytes(line, 'UTF-8'))
+
+    f.close()
+    write_buffer.close()
